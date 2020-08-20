4cab593aa2620cefe2701c613e6dafc46b258afa
Jade Pennig <jpennig@salesforce.com>
2020-08-20 13:54:35 -0500

    Update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const headerComment = require('gulp-header-comment')
+const Fiber = require('fibers')
 
-gulp.task('sass', function(cb) {
+// Explicitly declare dart-sass as the sass compiler
+sass.compiler = require('sass')
+
+/**
+ * Compiles Sass from ui/modules
+ */
+function compileSass() {
   return gulp
     .src('ui/modules/**/*.scss')
     .pipe(
       sass({
+        fiber: Fiber,
         sourceComments: false,
-        outputStyle: 'compressed'
-      })
+      }).on('error', sass.logError)
     )
-    .pipe(
-      headerComment(
-        '© Salesforce.com. \n FILE IS AUTOMATICALLY GENERATED from .scss'
-      )
-    )
-    .pipe(gulp.dest('ui/modules'))
+    .pipe(gulp.dest('./ui/modules'))
+}
 
-  cb()
-})
+/**
+ * Watch task to fire off different build tasks when certain directories change
+ */
+function watch() {
+  gulp.watch('ui/modules/**/*.scss', compileSass)
+}
 
-gulp.task('watch', function() {
-  gulp.watch('ui/modules/**/*.scss', gulp.series(['sass']))
-})
+/**
+ * Exporting public methods for external invocation
+ */
+exports.compileSass = compileSass
+exports.build = gulp.series(compileSass) // more will likely happen here
+exports.watch = watch
