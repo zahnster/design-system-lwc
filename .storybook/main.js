da256102b1ecb20668313a6e017c3265d2a0f54d
Jade Pennig <jpennig@salesforce.com>
2020-08-21 13:40:18 -0500

    Storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,15 @@ const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin')
 
 module.exports = {
   stories: ['../ui/modules/**/*.stories.js'],
-  addons: ['@storybook/addon-docs/register'],
+  addons: ['@storybook/addon-docs'],
 
-  webpackFinal: config => {
+  webpackFinal: (config) => {
     config.plugins.push(
       new LWCWebpackPlugin({
         namespace: {
           // LWC Namespace with path
-          c: path.resolve('./ui/modules')
-        }
+          c: path.resolve('./ui/modules'),
+        },
       })
     )
 
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.stories\.js$/,
         loaders: [require.resolve('@storybook/source-loader')],
-        enforce: 'pre'
+        enforce: 'pre',
       },
       {
         test: /\.mdx$/,
@@ -28,24 +28,24 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              plugins: ['@babel/plugin-transform-react-jsx']
-            }
+              plugins: ['@babel/plugin-transform-react-jsx'],
+            },
           },
           {
             loader: '@mdx-js/loader',
             options: {
-              compilers: [createCompiler({})]
-            }
-          }
-        ]
+              compilers: [createCompiler({})],
+            },
+          },
+        ],
       },
       {
         test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
         loader: 'file-loader/dist/cjs.js',
-        query: { name: 'static/media/[name].[hash:8].[ext]' }
-      }
+        query: { name: 'static/media/[name].[hash:8].[ext]' },
+      },
     ]
 
     return config
-  }
+  },
 }
